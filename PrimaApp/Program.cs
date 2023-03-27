8a5b57eed65a09249d57e1dd6c1624d49e2cc89a
Daniele Simoncini <simoncini@codeninja.it>
2023-03-27 12:35:05 +0200

    Aggiungere i file di progetto.

---
+++ PrimaApp/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.Clear();
+        string? nome;
+        int slm = int.Parse("1");
+        Console.WriteLine("Come ti chiami?");
+        nome = Console.ReadLine();
+        Console.WriteLine("Ciao " + nome + "!");
+    }
+}
