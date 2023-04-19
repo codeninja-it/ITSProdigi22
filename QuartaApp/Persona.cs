d0a7c820fca162cfee95c9349f8d8fa190e20f87
Daniele Simoncini <simoncini@codeninja.it>
2023-04-19 15:55:52 +0200

    parametri come valore o come riferimento, datatype e gettype per analizzare le strutture non conosciute

---
+++ QuartaApp/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartaApp
+{
+    [Table("persone")]
+    internal class Persona
+    {
+        [Key]
+        public int idPersona { get; set; }
+        [Required]
+        public string nome { get; set; }
+        [Required]
+        public string cognome { get; set; }
+        public int età { get; set; }
+
+        public DateTime creazione { get; } = DateTime.Now;
+
+        public Persona(string nome, string cognome, int età)
+        {
+            this.nome= nome;
+            this.cognome= cognome;
+            this.età = età;
+        }
+    }
+}
---
+++ QuartaApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+
+namespace QuartaApp
+{
+    internal class Program
+    {
+        static private List<Persona> persone = new List<Persona>();
+
+
+        static void Main(string[] args)
+        {
+            // esempio di riferimento
+            int prima = 1;
+            int seconda = 2;
+            somma(ref prima, seconda);
+            Console.WriteLine(prima);
+
+            // esempio di somma delle età
+            persone.Add( new Persona("Jhon", "Doe", 18)   );
+            persone.Add( new Persona("Federico", "Rossi", 20)   );
+
+            Persona nuova = new Persona(persone[0].nome, persone[0].cognome, persone[0].età);
+            nuova.età = 65;
+
+            Clona(nuova);
+
+            int totale = sommaEtà( persone[0], persone[1] );
+            Console.WriteLine($"totale delle età è {totale}");
+            Console.WriteLine($"Perchè Jhon ha {persone[0].età} mentre Federico ha {persone[1].età}");
+        }
+
+        static int sommaEtà(Persona prima, Persona seconda)
+        {
+            prima.età += seconda.età;
+            return prima.età;
+        }
+
+        static void somma(ref int a, int b)
+        {
+            a = a + b;
+            Console.WriteLine($"Sto stampando a: {a}");
+        }
+
+        //Clona(Persona persona);
+        static void Clona(object oggetto)
+        {
+            Type tipo = oggetto.GetType();
+            PropertyInfo[] props = tipo.GetProperties();
+            foreach ( PropertyInfo pi in props )
+            {
+                Console.Write($"Dimmi {pi.Name} : ");
+                var appoggio = Console.ReadLine();
+            }
+        }
+
+    }
+}
