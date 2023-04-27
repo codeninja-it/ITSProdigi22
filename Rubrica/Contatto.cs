15177cd05917588af89423bb7a54d6e43c097266
Daniele Simoncini <simoncini@codeninja.it>
2023-04-27 16:34:56 +0200

    rubrica 2

--- Rubrica/Contatto.cs
+++ Rubrica/Contatto.cs
@@ -8,9 +8,25 @@ namespace Rubrica
 {
     internal class Contatto
     {
+        public static int quanti { get; set; }
+        public int idContatto { get; }
         public string nome { get; set; }
         public string cognome { get; set; }
         public string telefono { get; set; }
         public DateTime creazione { get; } = DateTime.Now;
+
+        public Contatto(string nome, string cognome, string telefono)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.telefono = telefono;
+            quanti++;
+            this.idContatto = quanti;
+        }
+
+        public override string ToString()
+        {
+            return $"{nome}\t{cognome}\t{telefono}\t{creazione}";
+        }
     }
 }
--- Rubrica/Program.cs
+++ Rubrica/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Rubrica
+﻿using System.Text.Json;
+
+namespace Rubrica
 {
     internal class Program
     {
@@ -12,18 +14,53 @@
                 comando = chiedi("cosa vuoi fare?");
                 switch (comando)
                 {
-                    case "nuovo":
+                    case "nuovo": // Create
+                        contatti.Add(
+                                new(
+                                        chiedi("Nome", false),
+                                        chiedi("Cognome", false),
+                                        chiedi("Telefono", false)
+                                    )
+                            );
+                        Console.WriteLine($"contatti presenti: {Contatto.quanti}");
+                        break;
+
+                    case "vedi": // Read
+                        string chi = chiedi("Chi devo cercare", false);
+                        List<Contatto> selezionati = contatti.Where(singolo => singolo.nome.Contains(chi)).ToList();
+                        
+                        Contatto nuovo;
+
+                        foreach(Contatto singolo in selezionati)
+                        {
+                            Console.WriteLine(singolo);
+                        }
                         break;
                     case "cancella":
                         break;
+
                     case "modifica":
                         break;
-                    case "vedi":
-                        break;
+                    
                     case "salva":
+                        File.WriteAllText( "rubrica.json", JsonSerializer.Serialize(contatti) );
+                        Console.WriteLine("Rubrica salvata sul disco!");
                         break;
+
                     case "apri":
+                        try
+                        {
+                            string buffer = File.ReadAllText("rubrica.json");
+                            contatti = JsonSerializer.Deserialize<List<Contatto>>(buffer);
+                            Console.WriteLine($"caricati {contatti.Count} contatti");
+                        }
+                        catch
+                        {
+                            contatti = new();
+                            Console.WriteLine("Errore di caricamento del file...");
+                        }
                         break;
+
                     default:
                         Console.WriteLine("non ho capito...");
                         break;
@@ -32,12 +69,35 @@
             } while (comando != "exit");
         }
 
-        static private string chiedi(string domanda)
+        static private string chiedi(string domanda, bool corretto = true)
         {
             Console.Write($"{domanda} : ");
-            return Console.ReadLine().ToLower().Trim();
+            if (corretto)
+                return Console.ReadLine().ToLower().Trim();
+            else
+                return Console.ReadLine();
         }
 
 
     }
 }
+
+//l110:
+//int iVecchio = 0;
+//if(iVecchio < selezionati.Count)
+//{
+//    iVecchio++;
+//    goto l110;
+//}
+
+//int i = 0;
+//while(i < selezionati.Count)
+//{
+//    Contatto singolo = selezionati[i];
+//    i++;
+//}
+
+//for(int n = 0; n < selezionati.Count; n++)
+//{
+//    Contatto singolo = selezionati[n];
+//}
