a0d41ffd0c78feeb37c111535af284ff0eb08e6b
Daniele Simoncini <simoncini@codeninja.it>
2023-05-17 17:54:07 +0200

    termine web server

--- WebServer/Form1.Designer.cs
+++ WebServer/Form1.Designer.cs
@@ -29,31 +29,87 @@
 		private void InitializeComponent()
 		{
 			btnAvvia = new Button();
+			txtUrl = new TextBox();
+			txtRoot = new TextBox();
+			btnScegli = new Button();
+			dlgCartella = new FolderBrowserDialog();
+			lstRichieste = new ListBox();
 			SuspendLayout();
 			// 
 			// btnAvvia
 			// 
-			btnAvvia.Location = new Point(333, 124);
+			btnAvvia.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			btnAvvia.Location = new Point(12, 392);
 			btnAvvia.Name = "btnAvvia";
-			btnAvvia.Size = new Size(150, 46);
+			btnAvvia.Size = new Size(623, 46);
 			btnAvvia.TabIndex = 0;
 			btnAvvia.Text = "avvia";
 			btnAvvia.UseVisualStyleBackColor = true;
 			btnAvvia.Click += btnAvvia_Click;
 			// 
+			// txtUrl
+			// 
+			txtUrl.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			txtUrl.Location = new Point(12, 12);
+			txtUrl.Name = "txtUrl";
+			txtUrl.Size = new Size(623, 39);
+			txtUrl.TabIndex = 1;
+			txtUrl.Text = "http://127.0.0.1:8080/";
+			// 
+			// txtRoot
+			// 
+			txtRoot.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			txtRoot.Location = new Point(12, 61);
+			txtRoot.Name = "txtRoot";
+			txtRoot.ReadOnly = true;
+			txtRoot.Size = new Size(532, 39);
+			txtRoot.TabIndex = 2;
+			// 
+			// btnScegli
+			// 
+			btnScegli.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+			btnScegli.Location = new Point(550, 57);
+			btnScegli.Name = "btnScegli";
+			btnScegli.Size = new Size(85, 46);
+			btnScegli.TabIndex = 3;
+			btnScegli.Text = "...";
+			btnScegli.UseVisualStyleBackColor = true;
+			btnScegli.Click += btnScegli_Click;
+			// 
+			// lstRichieste
+			// 
+			lstRichieste.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			lstRichieste.FormattingEnabled = true;
+			lstRichieste.IntegralHeight = false;
+			lstRichieste.ItemHeight = 32;
+			lstRichieste.Location = new Point(12, 109);
+			lstRichieste.Name = "lstRichieste";
+			lstRichieste.Size = new Size(623, 277);
+			lstRichieste.TabIndex = 4;
+			// 
 			// Form1
 			// 
 			AutoScaleDimensions = new SizeF(13F, 32F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(800, 450);
+			ClientSize = new Size(647, 450);
+			Controls.Add(lstRichieste);
+			Controls.Add(btnScegli);
+			Controls.Add(txtRoot);
+			Controls.Add(txtUrl);
 			Controls.Add(btnAvvia);
 			Name = "Form1";
 			Text = "Form1";
 			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
 
 		private Button btnAvvia;
+		private TextBox txtUrl;
+		private TextBox txtRoot;
+		private Button btnScegli;
+		private FolderBrowserDialog dlgCartella;
+		private ListBox lstRichieste;
 	}
 }
--- WebServer/Form1.cs
+++ WebServer/Form1.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Net;
 using System.Text;
 
@@ -8,20 +9,63 @@ namespace WebServer
 		public Form1()
 		{
 			InitializeComponent();
+			/*
+			 * 1) permettere all'utente di selezionare la cartella di root
+			 * 2) attendere una chiamata
+			 * 3) capire quale pagina è richiesta
+			 * 4) se presente spedirla
+			 * 5) rimettersi in attesa della prossima chiamata
+			 * 6) opzionale: permettere l'uso di pagina php
+			 */
 		}
 
 		private void btnAvvia_Click(object sender, EventArgs e)
 		{
 			HttpListener server = new HttpListener();
-			server.Prefixes.Add("http://127.0.0.1:8080/"); // ...
+			server.Prefixes.Add(txtUrl.Text); // http://127.0.0.1:8080/
 			server.Start();
-			HttpListenerContext richiesta = server.GetContext();
-			//richiesta.Request.Url;
-			string messaggio = $"sito in costruzione: {richiesta.Request.RawUrl}";
-			byte[] pacchetto = Encoding.UTF8.GetBytes(messaggio);
-			richiesta.Response.OutputStream.Write(pacchetto);
-			richiesta.Response.OutputStream.Close();
+			while (true)
+			{
+				HttpListenerContext richiesta = server.GetContext();
+				string fileRichiesto = richiesta.Request.RawUrl.Substring(1);
+				lstRichieste.Items.Add(richiesta.Request.UserHostAddress + "\t" + fileRichiesto);
+				fileRichiesto = Path.Combine(txtRoot.Text, fileRichiesto);
+				byte[] pacchetto;
+				if (File.Exists(fileRichiesto))
+				{
+					if (fileRichiesto.EndsWith(".php"))
+					{
+						Process php = new Process();
+						php.StartInfo.FileName = @"c:\xampp\php\php.exe";
+						php.StartInfo.Arguments = fileRichiesto;
+						php.StartInfo.CreateNoWindow = true;
+						php.StartInfo.RedirectStandardOutput = true;
+						php.Start();
+						string interpretato = php.StandardOutput.ReadToEnd();
+						pacchetto = Encoding.UTF8.GetBytes(interpretato);
+					}
+					else
+					{
+						pacchetto = File.ReadAllBytes(fileRichiesto);
+					}
+				}
+				else
+				{
+					string messaggio = $"<html><body><b>Attenzione:</b> file non trovato: {richiesta.Request.RawUrl}</body></html>";
+					pacchetto = Encoding.UTF8.GetBytes(messaggio);
+				}
+				Stream cornetta = richiesta.Response.OutputStream;
+				cornetta.Write(pacchetto);
+				cornetta.Close();
+				Application.DoEvents();
+			}
 			server.Close();
+		}
+
+		private void btnScegli_Click(object sender, EventArgs e)
+		{
+			if (dlgCartella.ShowDialog() == DialogResult.OK)
+				txtRoot.Text = dlgCartella.SelectedPath;
 		}
 	}
 }
