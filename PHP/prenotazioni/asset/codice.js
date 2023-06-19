3e19a353ea627b6cdddebb9b5f255df152dd1395
Daniele Simoncini <simoncini@codeninja.it>
2023-06-19 16:42:31 +0200

    ultima versione con ajax riportato a sincrono

--- PHP/prenotazioni/asset/codice.js
+++ PHP/prenotazioni/asset/codice.js
@@ -1,74 +1,99 @@
 class tacciuinoElettronico {
-	constructor(tavoli, piatti, pulsante, tabella, totali){
+	constructor(tavoli, tipi, piatti, pulsante, tabella, totali, sedi) {
+		// recupero i riferimenti
+		this.selSedi = document.getElementById(sedi);
 		this.selTavoli = document.getElementById(tavoli);
+		this.selTipi = document.getElementById(tipi);
 		this.selPiatti = document.getElementById(piatti);
 		this.tabella = document.getElementById(tabella);
 		this.totali = document.getElementById(totali);
-		document.getElementById(pulsante).addEventListener("click", this.click.bind(this) );
-		this.init();
-		setInterval(this.refresh.bind(this), 5000);
+
+		// definisco gli eventi
+		this.selSedi.addEventListener("change", this.aggiornaTavoli.bind(this));
+		this.selTavoli.addEventListener("change", this.aggiornaTipi.bind(this));
+		this.selTipi.addEventListener("change", this.aggiornaPiatti.bind(this));
+
+		// definisco anche il pulsante di add di cui non ho bisogno di riferimenti
+		document.getElementById(pulsante).addEventListener("click", this.click.bind(this));
+
+		this.aggiornaSedi();
 	}
-	
-	chiamaServer(endPoint, callable){
+
+	// utility che chiama il server ed esegue sui dati una callable ricevuta come parametro
+	chiamaServer(endPoint, callable) {
 		let telefono = new XMLHttpRequest();
 		let baseUrl = "endpoint.php?";
 		telefono.open("GET", baseUrl + endPoint, true);
-		telefono.onreadystatechange = function(){
-			if(telefono.status == 200 && telefono.readyState == 4){
+		telefono.onreadystatechange = function () {
+			if (telefono.status == 200 && telefono.readyState == 4) {
 				let dati = JSON.parse(telefono.response);
-				if(callable != undefined)
+				if (callable != undefined)
 					callable(dati);
 			}
-				
+
 		};
 		telefono.send();
 	}
-	
-	refresh(){
-		// chiedere al server i dati del tavolo selezionato
-		this.chiamaServer("mod=list&id=" + this.selTavoli.value, function(dati){
-			let righe = "";
-			let totale = 0;
-			for(let n in dati){
-				let riga = dati[n];
-				righe += "<tr style=''>" +
-							"<td>" + riga["piatto"] + "</td>" +
-							"<td align='right'>" + riga["prezzo"] + "</td>" +
-						"</tr>";
-				totale += riga["prezzo"];
+
+	aggiornaSedi() {
+		this.chiamaServer("mod=sedi", function (dati) {
+			let buffer = "";
+			for (let i = 0; i < dati.length; i++) {
+				let riga = dati[i];
+				buffer += "<option value='" + riga["idsede"] + "'>" + riga["sede"] + "</option>";
 			}
-			this.tabella.innerHTML = righe;
-			this.totali.innerHTML = totale;
+			this.selSedi.innerHTML = buffer;
+			this.selSedi.value = dati[0]["idsede"];
+			this.aggiornaTavoli();
 		}.bind(this));
 	}
-	
-	init(){
-		this.chiamaServer("mod=tavoli", function(dati){
-			let buffer = "<option value='0'>Tutti</option>";
-			for(let n in dati){
-				let tavolo = dati[n];
-				buffer += "<option value='" + tavolo["idtavolo"] + "'>" + tavolo["tavolo"] + "</option>";
+
+	aggiornaTavoli() {
+		this.chiamaServer("mod=tavoli&idsede=" + this.selSedi.value, function (dati) {
+			let buffer = "";
+			for (let i = 0; i < dati.length; i++) {
+				let riga = dati[i];
+				buffer += "<option value='" + riga["idtavolo"] + "'>" + riga["tavolo"] + "</option>";
 			}
 			this.selTavoli.innerHTML = buffer;
+			this.selTavoli.value = dati[0]["idtavolo"];
+			this.aggiornaTipi();
 		}.bind(this));
-		this.chiamaServer("mod=piatti", function(dati){
+	}
+
+	aggiornaTipi() {
+		this.chiamaServer("mod=tipi&idtavolo=" + this.selTavoli.value, function (dati) {
 			let buffer = "";
-			for(let n in dati){
+			for (let i = 0; i < dati.length; i++) {
+				let riga = dati[i];
+				buffer += "<option value='" + riga["idtipo"] + "'>" + riga["tipo"] + "</option>";
+			}
+			this.selTipi.innerHTML = buffer;
+			this.selTipi.value = dati[0]["idtipo"];
+			this.aggiornaPiatti();
+		}.bind(this));
+	}
+
+	aggiornaPiatti() {
+		this.chiamaServer("mod=piatti&idtipo=" + this.selTipi.value + "&idtavolo=" + this.selTavoli.value, function (dati) {
+			let buffer = "";
+			for (let n in dati) {
 				let piatto = dati[n];
-				buffer += "<option value='" + piatto["idpiatto"] + "'>" + piatto["piatto"] + "</option>";
+				buffer += "<option value='" + piatto["idpiatto"] + "'>" + piatto["piatto"] + " €" + piatto["prezzo"] + "</option>";
 			}
 			this.selPiatti.innerHTML = buffer;
 		}.bind(this));
 	}
-	
-	click(){
-		this.chiamaServer("mod=add" + 
-							"&idpiatto=" + this.selPiatti.value + 
-							"&idtavolo=" + this.selTavoli.value);
+
+	click() {
+		this.chiamaServer("mod=add" +
+			"&idpiatto=" + this.selPiatti.value +
+			"&idtavolo=" + this.selTavoli.value);
+		this.refresh();
 	}
 }
 
 
-window.addEventListener("load", function(){
-	new tacciuinoElettronico("selTavolo", "selPiatto", "btnRegistra", "tblComande", "totPrezzo");
+window.addEventListener("load", function () {
+	new tacciuinoElettronico("selTavolo", "selTipo", "selPiatto", "btnRegistra", "tblComande", "totPrezzo", "selSede");
 });
--- PHP/prenotazioni/endpoint.php
+++ PHP/prenotazioni/endpoint.php
@@ -22,7 +22,7 @@
 	$buffer = array();
 	switch($_GET["mod"]){
 		case "list":
-			$buffer = EseguiDB("SELECT rst_piatti.piatto, rst_piatti.prezzo, rst_comande.fatto
+			$buffer = EseguiDB("SELECT rst_comande.idcomanda, rst_piatti.piatto, rst_comande.prezzo, rst_comande.fatto
 								FROM rst_comande
 								left join rst_piatti on rst_comande.idpiatto=rst_piatti.idpiatto ".
 								(intval($_GET["id"]) > 0 
@@ -31,16 +31,43 @@
 								);
 		break;
 		
-		case "piatti":
-			$buffer = EseguiDB("SELECT idpiatto, piatto 
-								FROM rst_piatti");
+		case "consegna": // ?mod=consegna&id=1
+			EseguiDB("UPDATE rst_comande
+						SET fatto = not fatto
+						WHERE idcomanda=".intval($_GET["id"]));
 		break;
 		
-		case "tavoli":
+		case "sedi": // ?mod=sedi
+			$buffer = EseguiDB("SELECT idsede, sede
+								FROM rst_sedi");
+		break;
+		
+		case "tavoli": // ?mod=tavoli&idsede=1
 			$buffer = EseguiDB("SELECT idtavolo, tavolo 
-								FROM rst_tavoli");
+								FROM rst_tavoli
+								WHERE idsede=".intval($_GET["idsede"]));
 		break;
 		
+		case "tipi": //?mod=tipi&idtavolo=1
+			$buffer = EseguiDB("SELECT rst_tipipiatti.idtipo, rst_tipipiatti.tipo
+								FROM rst_tavoli
+								LEFT JOIN rst_prezzi ON rst_tavoli.idsede=rst_prezzi.idsede
+								LEFT JOIN rst_piatti ON rst_prezzi.idpiatto=rst_piatti.idpiatto
+								LEFT JOIN rst_tipipiatti ON rst_piatti.idtipo=rst_tipipiatti.idtipo
+								WHERE rst_tavoli.idtavolo=".intval($_GET["idtavolo"])."
+								GROUP BY rst_tipipiatti.idtipo;");
+		break;
+
+		case "piatti": // ?mod=piatti&idtipo=1&idtavolo=1
+			$buffer = EseguiDB("SELECT rst_piatti.idpiatto, rst_piatti.piatto, rst_prezzi.prezzo
+								FROM rst_piatti
+								left join rst_prezzi on rst_piatti.idpiatto=rst_prezzi.idpiatto
+								left join rst_sedi on rst_prezzi.idsede=rst_sedi.idsede
+								left join rst_tavoli on rst_sedi.idsede=rst_tavoli.idsede
+								WHERE rst_piatti.idtipo=".intval($_GET["idtipo"])."
+								and rst_tavoli.idtavolo=".intval($_GET["idtavolo"]));
+		break;		
+				
 		case "add":
 			EseguiDB("INSERT INTO rst_comande (idtavolo, idpiatto, registrazione, 
 												fatto)
